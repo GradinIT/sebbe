137378122d4676c97aa73b75cbc9574dc7dcfe55
Joakim Gradin <joakimgradin@neo.local>
2022-12-07 15:31:08 +0100

    updated code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,6 @@
 package org.example;
 
-import org.example.exercise.Exercise1;
-import org.example.exercise.Exercise10;
-import org.example.exercise.Exercise14;
-import org.example.exercise.Exercise16;
-import org.example.exercise.Exercise17;
-import org.example.exercise.Exercise18;
-import org.example.exercise.Exercise2;
-import org.example.exercise.Exercise20;
-import org.example.exercise.Exercise22;
-import org.example.exercise.Exercise23;
-import org.example.exercise.Exercise3A;
-import org.example.exercise.Exercise3B;
-import org.example.exercise.RandomDice;
+import org.example.exercise.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,9 +9,10 @@ public class Main {
     public static void main(String[] args) {
         List<Exercise> exercises = Arrays.asList(
                 new Exercise1(),
-                new Exercise2()
- /*               new Exercise3A(),
-                new Exercise3B(),
+                new Exercise2(),
+                new Exercise3(),
+                new Exercise4()
+  /*
                 new Exercise10(),
                 new Exercise14(),
                 new Exercise16(),
--- src/main/java/org/example/exercise/Exercise3.java
+++ src/main/java/org/example/exercise/Exercise3.java
@@ -3,15 +3,17 @@ package org.example.exercise;
 import org.example.Exercise;
 import org.example.util.TextUtil;
 
-public class Exercise3A implements Exercise {
+public class Exercise3 implements Exercise {
     @Override
     public void run() {
+        printInformation();
         //System.out.println(Vad_är_det_som_är_fel_här?");
-        System.out.println("Vad_är_det_som_är_fel_här?"); //lösning
+        System.out.println("lösning: System.out.println(\"Vad_är_det_som_är_fel_här?\")"); //lösning
     }
 
     @Override
     public void printInformation() {
         System.out.println(TextUtil.underLineText("3. Ja? Vad är det som är fel med programmet? Lös felet och kör programmet.\n"));
+        System.out.println("System.out.println(Vad_är_det_som_är_fel_här?\");");
     }
 }
