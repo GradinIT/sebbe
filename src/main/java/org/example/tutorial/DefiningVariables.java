b711759670fbd90dcb599c93c8c2607d345b1e0d
Joakim Gradin <joakimgradin@neo.local>
2022-12-25 11:06:06 +0100

    update

--- src/main/java/org/example/tutorial/DefiningVariables.java
+++ src/main/java/org/example/tutorial/DefiningVariables.java
@@ -4,6 +4,7 @@ public class DefiningVariables {
     public static void main(String[] args) {
         int myInt = 1; // defining a variable of typ int with name myInt and assigning it the value of 1.
         double myDouble = 1.00; // defining a variable of typ double with name myDouble and assigning it the value of 1.00
+        short myShort =-32000;
         float myFloat = (float)1.235;
         byte myByte = 1;
         char myChar = 'H';
@@ -16,5 +17,6 @@ public class DefiningVariables {
         System.out.println(myBoolean);
         System.out.println(myChar);
         System.out.println(myString);
+        System.out.println(myShort);
     }
 }
